0752cb6f6809aebc32b83db1559110046e06cb51
Jodee Harris <jodee_harris@yahoo.com>
2023-07-18 02:06:47 -0500

    push 2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,65 +16,63 @@ to the terminal and export a text file with the results.
 import os
 import csv
 
-poll = []
 
 csvpath = os.path.join ("Resources", "election_data.csv")
 
-with open(csvpath) as csvfile:
+polls = []
+
+with open(csvpath,'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
     
     csv_header = next(csvreader)
-
-
-print("Election Results")
-
-print("-------------------------")
-
-rowcount = 0
-
-for row in csvreader:
-        rowcount += 1
-    
-print("Total Votes: " + str(rowcount))
-
-print("-------------------------")
-
-for polls in csvreader:
-    
-    politician_vote = int(poll[2])
+        
+    rowcount = 0
+    politician_vote = 0
     counterstockham = 0
     counterdegette = 0
     counterdoane = 0
-
-    if politician_vote == "Charles Casper Stockham":
-        counterstockham == counterstockham + 1
+    x = 0
+    
+    for row in csvreader:
+         rowcount += 1
+         
+    for politician_vote in csvreader:
+        politician_vote = len((polls[2]))
         
-    if politician_vote == "Diana DeGette":
-        counterdegette == counterdegette + 1
+        if politician_vote == "Charles Casper Stockham":
+            counterstockham = counterstockham + 1
+            
         
-    if politician_vote == "Raymon Anthony Doane":
-        counterdoane == counterdoane + 1
+        if politician_vote == "Diana DeGette":
+            counterdegette = counterdegette + 1
+        
+        if politician_vote == "Raymon Anthony Doane":
+            counterdoane = counterdoane + 1
         
 per_stockham = (counterstockham / rowcount) * 100
 per_degette = (counterdegette / rowcount) * 100
 per_doane = (counterdoane / rowcount) * 100
     
+if counterstockham > counterdoane and counterstockham > counterdegette:
 
-for winner in politician_vote:
+    x = str((f"Winner: Charles Casper Stockham"))
+
+elif counterdegette > counterstockham and counterdegette > counterdoane:
     
-    if counterstockham > counterdoane and counterstockham > counterdegette:
+    x = str((f"Winner: Diana DeGette"))
 
-        
-       x = print(f"Winner: Charles Casper Stockham")
-
-    if counterdegette > counterstockham and counterdegette > counterdoane:
+elif counterdoane > counterstockham and counterdoane > counterdegette:
     
-       x = print(f"Winner: Diana DeGette")
+    x = str((f"Winner: Raymon Anthony Doane"))
 
-    if counterdoane > counterstockham and counterdoane > counterdegette:
-    
-       x = print(f"Winner: Raymon Anthony Doane")
+print("Election Results")
+
+print("-------------------------")
+
+print("Total Votes: " + str(rowcount))
+
+print("-------------------------")
 
 print(f"Charles Casper Stockham:{per_stockham}% {counterstockham}")
 print(f"Diana DeGette: {per_degette}% {counterdegette}")
@@ -82,7 +80,7 @@ print(f"Raymon Anthony Doane:{per_doane}% {counterdoane}")
 
 print("-------------------------")
 
-
+print(x)
 
 print("-------------------------")
 
@@ -90,7 +88,7 @@ with open("Analysis/Info.txt", "w") as output_folder:
     
     output_folder.write("Election Results"+ "\n")
     output_folder.write("-------------------------" + "\n")
-    output_folder.write("Total Votes: " + rowcount + "\n")
+    output_folder.write("Total Votes: " + str(rowcount) + "\n")
     output_folder.write("-------------------------" + "\n")
     output_folder.write(f"Charles Casper Stockham:{per_stockham}% {counterstockham}\n")
     output_folder.write(f"Diana DeGette: {per_degette}% {counterdegette}\n")
