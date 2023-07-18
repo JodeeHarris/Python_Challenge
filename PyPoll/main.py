962cc1e46fe89acab8e1606125dcf5667e9c6e4d
Jodee Harris <jodee_harris@yahoo.com>
2023-07-18 16:27:55 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,19 @@
-""" 
-1. The total number of votes cast
-
-2. A complete list of candidates who received votes
-
-3. The percentage of votes each candidate won
-
-4. The total number of votes each candidate won
-
-5.The winner of the election based on popular vote 
-
-6. In addition, your final script should both print the analysis 
-to the terminal and export a text file with the results.
-
-"""
 import os
 import csv
 
-
+#places the path to a value
 csvpath = os.path.join ("Resources", "election_data.csv")
 
+#opens the csvfile to allow the program to read the information
 with open(csvpath,'r') as csvfile:
-
+    
+    #The Act of reading the file with arguements on how to read the file
     csvreader = csv.reader(csvfile, delimiter=',')
     
+    #Skips the headers
     csv_header = next(csvreader)
-        
+    
+    # Placing the word values with a numerical value     
     rowcount = 0
     politician_vote = 0
     counterstockham = 0
@@ -32,23 +21,35 @@ with open(csvpath,'r') as csvfile:
     counterdoane = 0
     x = 0
     
+    # Row accesses our csv list
     for row in csvreader:
+        
+        #Counts total amount of rows in the csv
         rowcount += 1
+       
+        #Accessing the third column in our csv
         politician_vote = row[2]
-         
+        
+        #Placing conditional "if" statements to locate this value from column three 
         if politician_vote == "Charles Casper Stockham":
+           
+            #If value is found add one to the original value until the is no more values to check 
             counterstockham = counterstockham + 1
+        
         if politician_vote == "Diana DeGette":
             counterdegette = counterdegette + 1
+        
         if politician_vote == "Raymon Anthony Doane":
             counterdoane = counterdoane + 1
-        
+
+#Finding the Percentages of each candidate selected        
 per_stockham = (counterstockham / rowcount) * 100
 per_degette = (counterdegette / rowcount) * 100
 per_doane = (counterdoane / rowcount) * 100
     
+#Using "if" statements to locate which candidate had the most counted votes
 if counterstockham > counterdoane and counterstockham > counterdegette:
-
+    #Assigning a value to the output
     x = str((f"Winner: Charles Casper Stockham"))
 
 elif counterdegette > counterstockham and counterdegette > counterdoane:
@@ -59,6 +60,7 @@ elif counterdoane > counterstockham and counterdoane > counterdegette:
     
     x = str((f"Winner: Raymon Anthony Doane"))
 
+#Printing results to the terminal
 print("Election Results")
 
 print("-------------------------")
@@ -77,8 +79,10 @@ print(x)
 
 print("-------------------------")
 
+#Opening a path to a text file and writing in it with a value to address the folder
 with open("Analysis/Info.txt", "w") as output_folder:
     
+    #writing in the text file
     output_folder.write("Election Results"+ "\n")
     output_folder.write("-------------------------" + "\n")
     output_folder.write("Total Votes: " + str(rowcount) + "\n")
